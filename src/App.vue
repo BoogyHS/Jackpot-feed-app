dab44348640b268c2485ead277ff29f24c9a5174
Boogy83 <bozhidar.atanasov@abv.bg>
2020-11-11 08:45:05 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Wrapper></Wrapper>
+</template>
+
+<script>
+import Wrapper from './components/Wrapper.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Wrapper
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Wrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Test</div>
+</template>
+
+<script>
+export default {
+  name: 'Wrapper',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
